a42ec2d8c189f74f030f78aac20ad1f5fd3dfab3
donkim <donkim@gmail.com>
2024-12-06 06:09:44 +0900

    프로덕트 리스트 보여주기.

--- font/src/Pages/Product.jsx
+++ font/src/Pages/Product.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import ProductDiplay from '../Components/ProductDiplay/ProductDiplay';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
+import { ShopContext } from '../Context/ShopContext';
 
 const Product = () => {
+    const {all_product} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e)=>e.id === Number(productId));
   return (
-    <div>Product</div>
+    <div>
+        <Breadcrum product={product}/>
+        <ProductDiplay product={product}/>
+        <DescriptionBox/>
+        <RelatedProducts/>
+    </div>
   )
 }
 
